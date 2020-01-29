9a8c12a46a8801b0be2646dc7f55768e55cd5400
jagadeeswar <51435785+jagadeeswar300@users.noreply.github.com>
2020-01-29 12:39:03 +0530

    Create prog1.py

---
+++ prog1.py
@@ -0,0 +1,13 @@
+class section:
+    def __init__(self):
+        self.name = 'subhu'
+        self.age = 100
+        self.clas='e'
+        self.marks=99
+    def talk(self):
+        print('Hi,I AM ',self.name)
+        print('age is',self.age)
+        print('class is ',self.clas)
+        print('marks is',self.marks)
+s1=section()
+s1.talk()
